8c0ef8bc0297e9010270cf217115d862b3627523
Dhoniboy <103166082+Dhoniboy@users.noreply.github.com>
2023-02-11 21:13:25 +0530

    Update start.py
    Signed-off-by: Dhoniboy <103166082+Dhoniboy@users.noreply.github.com>

--- plugins/start.py
+++ plugins/start.py
@@ -16,17 +16,17 @@ async def start(client, message):
         await db.add_user(user.id)             
     txt=f"👋 Hello Developer {user.mention} \n\nI am an Advance file Renamer and file Converter BOT with Custom thumbnail support.\n\nSend me any video or document !"
     button=InlineKeyboardMarkup([[
-        InlineKeyboardButton(" Developer ", callback_data='dev')
+        InlineKeyboardButton(" Developer ", url='https://t.me/Movie_Zone_C')
         ],[
-        InlineKeyboardButton(' Updates', url='https://t.me/WebXBots'),
-        InlineKeyboardButton(' Support', url='https://t.me/Web_X_Support')
+        InlineKeyboardButton(' Updates', url='https://t.me/Movie_Zone_C'),
+        InlineKeyboardButton(' Support', url='https://t.me/Movie_Zone_C')
         ],[
         InlineKeyboardButton(' About', callback_data='about'),
         InlineKeyboardButton(' Help', callback_data='help')
         ],[
-        InlineKeyboardButton(" Join Our Movie Channel !", url='https://t.me/DCinemasz')
+        InlineKeyboardButton(" Join Our Movie Channel !", url='https://t.me/Movie_Zone_C')
         ],[
-        InlineKeyboardButton("❤️ Subscribe YT ❤️", url='https://www.youtube.com/@Divin_Dass')
+        InlineKeyboardButton("❤️ Subscribe YT ❤️", url='https://t.me/Movie_Zone_C')
         ]
         ])
     if START_PIC:
@@ -70,17 +70,17 @@ async def cb_handler(client, query: CallbackQuery):
         await query.message.edit_text(
             text=f"""👋 Hello Developer {query.from_user.mention} \n\nI am an Advance file Renamer and file Converter BOT with permanent and custom thumbnail support.\n\nSend me any video or document !""",
             reply_markup=InlineKeyboardMarkup( [[
-        InlineKeyboardButton(" Developer ", callback_data='dev')
+        InlineKeyboardButton(" Developer ", url='https://t.me/Movie_Zone_C')
         ],[
-        InlineKeyboardButton(' Updates', url='https://t.me/WebXBots'),
-        InlineKeyboardButton(' Support', url='https://t.me/Web_X_Support')
+        InlineKeyboardButton(' Updates', url='https://t.me/Movie_Zone_C'),
+        InlineKeyboardButton(' Support', url='https://t.me/Movie_Zone_C')
         ],[
         InlineKeyboardButton(' About', callback_data='about'),
         InlineKeyboardButton(' Help', callback_data='help')
         ],[
-        InlineKeyboardButton(" Join Our Movie Channel !", url='https://t.me/DCinemasz')
+        InlineKeyboardButton(" Join Our Movie Channel !", url='https://t.me/Movie_Zone_C')
         ],[
-        InlineKeyboardButton("❤️ Subscribe YT ❤️", url='https://www.youtube.com/@Divin_Dass')
+        InlineKeyboardButton("❤️ Subscribe YT ❤️", url='https://t.me/Movie_Zone_C')
         ]
         ]
                 )
@@ -89,7 +89,7 @@ async def cb_handler(client, query: CallbackQuery):
         await query.message.edit_text(
             text=mr.HELP_TXT,
             reply_markup=InlineKeyboardMarkup( [[
-               InlineKeyboardButton(" Join our Movie Channel ", url="https://t.me/DCinemasz")
+               InlineKeyboardButton(" Join our Movie Channel ", url="https://t.me/Movie_Zone_C")
                ],[
                InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
                InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data = "start")
@@ -101,7 +101,7 @@ async def cb_handler(client, query: CallbackQuery):
             text=mr.ABOUT_TXT.format(client.mention),
             disable_web_page_preview = True,
             reply_markup=InlineKeyboardMarkup( [[
-                InlineKeyboardButton(" Join our Movie Channel ", url="https://t.me/DCinemasz")
+                InlineKeyboardButton(" Join our Movie Channel ", url="https://t.me/Movie_Zone_C")
                ],[
                InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
                InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data = "start")
@@ -112,7 +112,7 @@ async def cb_handler(client, query: CallbackQuery):
         await query.message.edit_text(
             text=mr.DEV_TXT,
             reply_markup=InlineKeyboardMarkup( [[
-                InlineKeyboardButton(" Join our Movie Channel ", url="https://t.me/DCinemasz")
+                InlineKeyboardButton(" Join our Movie Channel ", url="https://t.me/Movie_Zone_C")
                ],[
                InlineKeyboardButton(" 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
                InlineKeyboardButton(" 𝙱𝙰𝙲𝙺", callback_data = "start")
